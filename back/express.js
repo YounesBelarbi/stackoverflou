45595f68f12c3ecc65dd61944d41054c1736fdbb
YounesBelarbi <ybelarbi.dev@gmail.com>
2022-02-28 21:24:56 +0100

    build: setting up body-parser and cors

--- back/express.js
+++ back/express.js
@@ -5,6 +5,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const cors = require('cors');
+app.use(cors());
+
 app.get('/', function (req, res, next) {
     res.json({status:200, msg:'success'});
 })
